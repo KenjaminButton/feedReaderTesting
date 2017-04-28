2e33fdbad4468ce2b90afc38f264d98f85a55737
KendrickPC <kennethpchang@gmail.com>
2017-04-28 13:34:18 +0800

    jasmine calculator test works perfectly

---
+++ jasmineCalculatorTesting/js/calculator.js
@@ -0,0 +1,24 @@
+var Calculator = function() {
+	 return {
+	 	add: function(a, b) {
+	 		return a + b;
+	 	}, 
+	 	subtract: function(a, b) {
+	 		return a - b;
+	 	},
+	 	multiply: function(a, b) {
+	 		return a * b;
+	 	},
+	 	divide: function(a, b) {
+	 		return a / b;
+	 	}
+	 }
+
+
+describe('calculatorSuite', function() {
+	var calculator = new Calculator();
+
+	it('should add two numbers together', function() {
+		expect(calculator.add(5,5)).toBe(10);
+	});
+});
