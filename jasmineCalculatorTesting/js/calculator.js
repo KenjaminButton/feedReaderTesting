288ac2d239ffd8c4d5032a4cab5b28c3652e87e1
KendrickPC <kennethpchang@gmail.com>
2017-04-28 13:45:29 +0800

    subtraction test working

--- jasmineCalculatorTesting/js/calculator.js
+++ jasmineCalculatorTesting/js/calculator.js
@@ -13,12 +13,15 @@ var Calculator = function() {
 	 		return a / b;
 	 	}
 	 }
-
+}
 
 describe('calculatorSuite', function() {
 	var calculator = new Calculator();
 
 	it('should add two numbers together', function() {
 		expect(calculator.add(5,5)).toBe(10);
+	});
+	it('should subtract two numbers', function() {
+		expect(calculator.subtract(90,8)).toBe(82);
 	});
 });
