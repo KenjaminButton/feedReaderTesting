974c849ff0e74488b3d65599bd73370e427d7d86
KendrickPC <kennethpchang@gmail.com>
2017-05-01 02:45:17 +0800

    SPEC HAS NO EXPECTATIONS URL is defined (after defining URL)

--- jasmine/spec/feedreader.js
+++ jasmine/spec/feedreader.js
@@ -3,9 +3,8 @@
  * This is the spec file that Jasmine will read and contains
  * all of the tests that will be run against your application.
  */
-/* We're placing all of our tests within the $() function,
- * since some of these tests may require DOM elements. We want
- * to ensure they don't run until the DOM is ready.
+/* All of my tests are within the $() function
+ * Ensure that tests won't run until the DOM is ready.
  */
 $(function() {
     /* This is our first test suite - a test suite just contains
@@ -13,13 +12,7 @@ $(function() {
      * feeds definitions, the allFeeds variable in our application.
      */
     describe('RSS Feeds', function() {
-        /* This is our first test - it tests to make sure that the
-         * allFeeds variable has been defined and that it is not
-         * empty. Experiment with this before you get started on
-         * the rest of this project. What happens when you change
-         * allFeeds in app.js to be an empty array and refresh the
-         * page?
-         */
+
         it('are defined', function() {
             expect(allFeeds).toBeDefined();
             expect(allFeeds.length).not.toBe(0);
@@ -28,6 +21,9 @@ $(function() {
          * in the allFeeds object and ensures it has a URL defined
          * and that the URL is not empty.
          */
+        it('URL is defined', function () {
+            
+        }) 
         /* TODO: Write a test that loops through each feed
          * in the allFeeds object and ensures it has a name defined
          * and that the name is not empty.
--- jasmineCalculatorTesting/js/calculator.js
+++ jasmineCalculatorTesting/js/calculator.js
@@ -1,33 +1,31 @@
 var Calculator = function() {
-	 return {
-	 	add: function(a, b) {
-	 		return a + b;
-	 	}, 
-	 	subtract: function(a, b) {
-	 		return a - b;
-	 	},
-	 	multiply: function(a, b) {
-	 		return a * b;
-	 	},
-	 	divide: function(a, b) {
-	 		return a / b;
-	 	}
-	 }
+	return {
+		add: function(a, b) {
+			return a + b;
+		},
+		subtract: function(a, b) {
+			return a - b;
+		},
+		multiply: function(a, b) {
+			return a * b;
+		},
+		divide: function(a, b) {
+			return a / b;
+		}
+	}
 }
-
 describe('calculatorSuite', function() {
 	var calculator = new Calculator();
-
 	it('should add two numbers together', function() {
-		expect(calculator.add(5,5)).toBe(10);
+		expect(calculator.add(5, 5)).toBe(10);
 	});
 	it('should subtract two numbers', function() {
-		expect(calculator.subtract(90,8)).toBe(82);
+		expect(calculator.subtract(90, 8)).toBe(82);
 	});
 	it('should multiply two numbers to get the product', function() {
-		expect(calculator.multiply(8,8)).toBe(64);
+		expect(calculator.multiply(8, 8)).toBe(64);
 	});
 	it('should divide two numbers to get the quotient', function() {
-		expect(calculator.divide(81,9)).toBe(9);
+		expect(calculator.divide(81, 9)).toBe(9);
 	});
 });
