1efee44f6fafbc898881c3dcdf692af98b2f5a37
KendrickPC <kennethpchang@gmail.com>
2017-05-03 22:12:19 +0800

    wrote a test to ensure when a new feed is loaded by the loadFeed function

--- README.md
+++ README.md
@@ -2,45 +2,23 @@
 # Test-Driven Development 
 ## The purpose is to use Jasmine to write a number of tests against pre-existing application. I am testing the underlying business logic of the application as well as the event handling and DOM manipulation.
 
-Install Jasmine
-	https://jasmine.github.io/
+Install Jasmine 2.0
+	<!-- https://jasmine.github.io/ -->
 	When allFeeds variable is undefined with an empty array, the results return a spec failure of "Expected 0 not to be 0.""
 
-1. Edit the `allFeeds` variable in **./js/app.js** to make the provided test fail and see how Jasmine visualizes this failure in your application.
+- Run local server with python -m SimpleHTTPServer 8000
+- Open web browser to localhost:8000
+- Run Jasmine test
 
-<!-- https://jasmine.github.io/2.0/introduction.html -->
+## Code Cleaned Up With 
+<!-- https://dirtymarkup.com/ -->
 
-	Error: at Object.<anonymous> (http://localhost:8080/jasmine/spec/feedreader.js:25:41)
+# Helpful Links:
 
-	    it('are defined', function() {
-	        expect(allFeeds).toBeDefined();
-	        expect(allFeeds.length).not.toBe(0);
-	    });
-
-2. Return the `allFeeds` variable to a passing state.
-
-
-3. Write a test that loops through each feed in the `allFeeds` object and ensures it has a URL defined and that the URL is not empty.
-
-
-4. Write a test that loops through each feed in the `allFeeds` object and ensures it has a name defined and that the name is not empty.
-5. Write a new test suite named `"The menu"`.
-6. Write a test that ensures the menu element is hidden by default. You'll have to analyze the HTML and the CSS to determine how we're performing the hiding/showing of the menu element.
-7. Write a test that ensures the menu changes visibility when the menu icon is clicked. This test should have two expectations: does the menu display when clicked and does it hide when clicked again.
-8. Write a test suite named `"Initial Entries"`.
-9. Write a test that ensures when the `loadFeed` function is called and completes its work, there is at least a single `.entry` element within the `.feed` container.
-10. Write a test suite named `"New Feed Selection"`.
-11. Write a test that ensures when a new feed is loaded by the `loadFeed` function that the content actually changes.
-12. No test should be dependent on the results of another.
-13. Callbacks should be used to ensure that feeds are loaded before they are tested.
-14. Implement error handling for undefined variables and out-of-bound array access.
-15. When complete - all of your tests should pass. 
-
-
-<!-- ## Setup and Teardown
-
-## To help a test suite DRY up any duplicated setup and teardown code, Jasmine provides the global beforeEach and afterEach functions. As the name implies, the beforeEach function is called once before each spec in the describe in which it is called, and the afterEach function is called once after each spec. Here is the same set of specs written a little differently. The variable under test is defined at the top-level scope -- the describe block -- and initialization code is moved into a beforeEach function. The afterEach function resets the variable before continuing. -->
-
-
-
-
+	hasClass implementation in Jasmine 2.0
+<!-- http://stackoverflow.com/questions/20268128/how-to-test-if-an-element-has-class-using-protractor -->
+<!-- http://stackoverflow.com/questions/32615713/tobetrue-vs-tobetruthy-vs-tobetrue -->
+	before and after async specs in Jasmine 2.0
+<!-- https://github.com/jasmine/jasmine/issues/526 -->
+	how to reuse beforeEach and afterEach in Jasmine 2.0
+<!-- http://stackoverflow.com/questions/17317839/how-to-reuse-beforeeach-aftereach-in-jasmine-js -->
--- jasmine/spec/feedreader.js
+++ jasmine/spec/feedreader.js
@@ -7,27 +7,25 @@
  * This is done to ensure that tests won't run until the DOM is ready.
  */
 $(function() {
-// This suite is all about the RSS
-// feeds definitions and the allFeeds variable in our application.
-    
-     // https://jasmine.github.io/2.0/introduction.html
-     describe('RSS Feeds', function() {
+    // This suite is all about the RSS
+    // feeds definitions and the allFeeds variable in our application.
+    // https://jasmine.github.io/2.0/introduction.html
+    describe('RSS Feeds', function() {
         it('are defined', function() {
             expect(allFeeds).toBeDefined();
             expect(allFeeds.length).not.toBe(0);
         });
-// Wrote a test that loops through each feed in the allFeeds object and ensures it has a URL defined and that the URL is not empty.  
-        it('url is defined', function () {
-            allFeeds.forEach(function (feed) {
+        // Wrote a test that loops through each feed in the allFeeds object and ensures it has a URL defined and that the URL is not empty.  
+        it('url is defined', function() {
+            allFeeds.forEach(function(feed) {
                 feedUrl = feed.url;
                 expect(feedUrl).toBeDefined();
                 expect(feedUrl.length).not.toBe(0);
             });
         });
-// Wrote a test that loops through each feed
-// in the allFeeds object and ensures it has a name defined
-// and that the name is not empty.
-        
+        // Wrote a test that loops through each feed
+        // in the allFeeds object and ensures it has a name defined
+        // and that the name is not empty.
         it('name is defined', function() {
             allFeeds.forEach(function(feed) {
                 feedName = feed.name;
@@ -36,79 +34,69 @@ $(function() {
             });
         });
     });
-/* Wrote a  test suite named "The menu" */
-    describe('The menu', function () {
+    /* Wrote a  test suite named "The menu" */
+    describe('The menu', function() {
         // test that ensures the menu element is hidden by default
         it('has an element hidden by default', function() {
-// hasClass implementation found here
-// http://stackoverflow.com/questions/20268128/how-to-test-if-an-element-has-class-using-protractor
-// http://stackoverflow.com/questions/32615713/tobetrue-vs-tobetruthy-vs-tobetrue
-        expect($('body').hasClass('menu-hidden')).toBeTruthy();
+            // hasClass implementation found here
+            // http://stackoverflow.com/questions/20268128/how-to-test-if-an-element-has-class-using-protractor
+            // http://stackoverflow.com/questions/32615713/tobetrue-vs-tobetruthy-vs-tobetrue
+            expect($('body').hasClass('menu-hidden')).toBeTruthy();
         });
-// Wrote a test to ensure the menu changes
-// visibility when the menu icon is clicked. This test
-// should have two expectations: does the menu display when
-// clicked and does it hide when clicked again.
+        // Wrote a test to ensure the menu changes
+        // visibility when the menu icon is clicked. This test
+        // should have two expectations: does the menu display when
+        // clicked and does it hide when clicked again.
         it('switches the visibility upon clicking on the icon', function() {
-// refers to index.html file line 29
+            // refers to index.html file line 29
             $('a.menu-icon-link').click();
             expect(document.body.className).not.toContain('menu-hidden');
-// refers to index.html file line 29            
+            // refers to index.html file line 29            
             $('a.menu-icon-link').click();
             expect(document.body.className).toContain('menu-hidden');
         });
     });
-
-// Wrote a new test suite named "Initial Entries"
-    describe('Initial Entries', function () {
-// Wrote a test that ensure when the loadFeed function is called and 
-// its work is complete. The loadFeed() is asynchronous so this test  
-// requires Jasmine's beforeEach & asynchronous done() function.
-// http://stackoverflow.com/questions/17317839/how-to-reuse-beforeeach-aftereach-in-jasmine-js
-        beforeEach(function (done) {
+    // Wrote a new test suite named "Initial Entries"
+    describe('Initial Entries', function() {
+        // Wrote a test that ensure when the loadFeed function is called and 
+        // its work is complete. The loadFeed() is asynchronous so this test  
+        // requires Jasmine's beforeEach & asynchronous done() function.
+        // http://stackoverflow.com/questions/17317839/how-to-reuse-beforeeach-aftereach-in-jasmine-js
+        beforeEach(function(done) {
             loadFeed(0, done);
         });
-        it('has something greater than zero 0', function (done) {
-// feed refers to line 35 in index.html and entry in line index.html line 38
-// not.toBe found from the following:
-// https://jasmine.github.io/2.0/introduction.html
+        it('has something greater than zero 0', function(done) {
+            // feed refers to line 35 in index.html and entry in line index.html line 38
+            // not.toBe found from the following:
+            // https://jasmine.github.io/2.0/introduction.html
             expect($('.feed .entry').length).not.toBe([0]);
             done();
         });
-
     });
-// Wrote a new test suite named "New Feed Selection"
+    // Wrote a new test suite named "New Feed Selection"
     describe('New Feed Selection', function() {
-// Store two the variable values for two feeds after loading
-    var feedPrior;
-    var feedLatter;
-
-    beforeEach(function (done) {
-// run before and after asynchronous specs in jasmine 2.0
-// https://github.com/jasmine/jasmine/issues/526
-// loadFeed function referenced in line 38 of app.js file handles async
-    loadFeed(0, function() {
-// refers to div class feed in line 35 of index.html file
-        feedPrior = $('.feed').html();
-// calls the feed asynchronously
-            done();
+        // Store two the variable values for two feeds after loading
+        var feedPrior;
+        var feedLatter;
+        beforeEach(function(done) {
+            // run before and after asynchronous specs in jasmine 2.0
+            // https://github.com/jasmine/jasmine/issues/526
+            // loadFeed function referenced in line 38 of app.js file handles async
+            loadFeed(0, function() {
+                // refers to div class feed in line 35 of index.html file
+                feedPrior = $('.feed').html();
+                // calls the feed asynchronously
+                done();
+            });
+        });
+        // wrote a test to ensure when a new feed is loaded by the loadFeed function
+        it('ensures new feed is loaded by loadFeed function', function(done) {
+            loadFeed(1, function() {
+                feedLatter = $('.feed').html();
+                expect(feedLatter).not.toBe(feedPrior);
+                // asynchronous loading handled below
+                done();
+            });
         });
     });
-
-    it('ensures new feed is loaded by loadFeed function', function (done) {
-        loadFeed(1, function () {
-            done();
-        });
-    });
-
-
-
-
-
-
-    });
-    /* TODO: Write a test that ensures when a new feed is loaded
-     * by the loadFeed function that the content actually changes.
-     * Remember, loadFeed() is asynchronous.
-     */
 }());
