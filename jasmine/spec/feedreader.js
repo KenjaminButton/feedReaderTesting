dfe0ede9014ac10d3af49146ba4e8a8f2488dd3b
KendrickPC <kennethpchang@gmail.com>
2017-05-03 17:31:16 +0800

    wrote a new test suite for New Feed Selection

--- jasmine/spec/feedreader.js
+++ jasmine/spec/feedreader.js
@@ -75,16 +75,13 @@ $(function() {
             expect($('.feed .entry').length).not.toBe([0]);
             done();
         });
-    });
 
-    
-    /* TODO: Write a test that ensures when the loadFeed
-     * function is called and completes its work, there is at least
-     * a single .entry element within the .feed container.
-     * Remember, loadFeed() is asynchronous so this test will require
-     * the use of Jasmine's beforeEach and asynchronous done() function.
-     */
-    /* TODO: Write a new test suite named "New Feed Selection" */
+    });
+// Wrote a new test suite named "New Feed Selection"
+    describe('New Feed Selection', function() {
+
+    })
+
     /* TODO: Write a test that ensures when a new feed is loaded
      * by the loadFeed function that the content actually changes.
      * Remember, loadFeed() is asynchronous.
